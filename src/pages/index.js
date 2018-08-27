db59627931f28f9148ab696380b2853cd8733e07
Marouane Rassili <mrassili@aol.com>
2018-08-27 21:55:42 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>I'm Marouane, 20yo from Morocco. I have a built applications using HTML, CSS, JS (ES6) and APIs. I'm excited for this new Online Hackathon journey!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
